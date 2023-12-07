28f884a39a95f8ce3420f92908f4f3ea000764c1
abuSaeedX <152357885+abuSaeedX@users.noreply.github.com>
2023-12-07 14:33:09 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function yes () {
+    document.getElementById("feedback").innerHTML="Thank you so much! It means a lot to me!";
+}
+
+function no () {
+    document.getElementById("feedback").innerHTML="Please let me know how can i improve myself, Thanks.";
+}
